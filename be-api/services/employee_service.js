7efd16ea66f9f03edc448b3507907e3b42d28354
Deepak Patidar <deepak@newput.com>
2017-07-26 17:44:29 +0530

    Fixed table name error

--- be-api/services/employee_service.js
+++ be-api/services/employee_service.js
@@ -3,7 +3,7 @@ var con = require('../../db/connection/connection');
 module.exports = {
 		getEmployeeService: function(reqData, callback) {
             //var req_query = "INSERT INTO employee (email, username, pass) VALUES ('sshubham@gmail.com', 'sTom B. Erichsen', 'sSkagen 21');"
-            var req_query = "select * from employeee;"
+            var req_query = "select * from employee;"
             con.query(req_query,function(err, result) {
             if(err) {
                 return callback(err)

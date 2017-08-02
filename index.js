187ba00ecd5ecf139b6017b910e7bce88472636f
Deepak Patidar <deepak@newput.com>
2017-08-02 15:30:26 +0530

    Fixed cross origin id issue

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*")
 	res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, device_id, device_token")
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token, device_id, device_token")
     next();
 });
 

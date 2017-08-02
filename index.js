b10a2aa5ce9223e49b84a3e6dcc03f77fe257f34
Deepak Patidar <deepak@newput.com>
2017-08-02 10:52:50 +0530

    Added cross origin

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*")
 	res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, device_id, device_token")
-}
+    next();
+});
 
 app.set('port', (process.env.PORT || 3000));
 console.log(app.get('port'))

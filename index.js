c08d76d55161d919787b1ffc57861c1c9d232894
Deepak Patidar <deepak@newput.com>
2017-08-02 10:46:41 +0530

    Added cross origin

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ process.env.SECRET_KEY="thisismysecretkey";
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*")
+	res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, device_id, device_token")
+}
+
 app.set('port', (process.env.PORT || 3000));
 console.log(app.get('port'))
 
